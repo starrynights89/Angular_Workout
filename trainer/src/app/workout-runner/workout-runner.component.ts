c7955b5445095684cfa4e8d43eb500260d8579c4
Alexander Hartson <alexander-hartson@live.com>
2019-03-09 17:09:25 -0600

    changing workout-runner.component

--- trainer/src/app/workout-runner/workout-runner.component.ts
+++ trainer/src/app/workout-runner/workout-runner.component.ts
@@ -43,10 +43,32 @@ export class WorkoutRunnerComponent implements OnInit {
     const intervalId = setInterval(() => {
       if (this.exerciseRunningDuration >= this.currentExercise.duration) {
         clearInterval(intervalId);
+        const next: ExercisePlan = this.getNextExercise();
+        if (next) {
+          if (next !== this.restExercise) {
+            this.currentExerciseIndex++;
+          }
+          this.startExercise(next);
+        } else {
+          console.log("Workout complete!");
+        }
       } else {
         this.exerciseRunningDuration++;
       }
     }, 1000);
+  }
+
+  getNextExercise(): ExercisePlan {
+    let nextExercise: ExercisePlan = null;
+    if (this.currentExercise === this.restExercise) {
+      nextExercise = this.workoutPlan.exercises[this.currentExerciseIndex + 1];
+    } else if (
+      this.currentExerciseIndex <
+      this.workoutPlan.exercises.length - 1
+    ) {
+      nextExercise = this.restExercise;
+    }
+    return nextExercise;
   }
 
   buildWorkout(): WorkoutPlan {
