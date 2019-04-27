43b3f975f78b76770ef0ba3947ac8db2cabd51d2
Alexander Hartson <alexander-hartson@live.com>
2019-04-27 16:32:24 -0500

    adding component

--- trainer/src/app/workout-runner/workout-runner.module.ts
+++ trainer/src/app/workout-runner/workout-runner.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { WorkoutRunnerComponent } from "./workout-runner.component";
+import { ExerciseDescriptionComponent } from './exercise-description/exercise-description.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [WorkoutRunnerComponent],
+  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent],
   // export WorkoutRunnerComponent to allow AppComponent to locate component
   exports: [WorkoutRunnerComponent]
 })
