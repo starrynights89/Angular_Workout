7bc891a15fb355ee3a3a65bb86deb4137adab462
Alexander Hartson <alexander@waffles.local>
2019-05-02 21:28:27 -0500

    setting up embeded youtube video

--- trainer/src/app/workout-runner/workout-runner.module.ts
+++ trainer/src/app/workout-runner/workout-runner.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { WorkoutRunnerComponent } from "./workout-runner.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkoutRunnerComponent } from './workout-runner.component';
 import { ExerciseDescriptionComponent } from './exercise-description/exercise-description.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent],
+  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent, VideoPlayerComponent],
   // export WorkoutRunnerComponent to allow AppComponent to locate component
   exports: [WorkoutRunnerComponent]
 })
